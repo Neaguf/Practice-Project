a2f9b5d9691f0b6b5157a99fc61094d9fe88ed83
Neagu Florin <112757160+Neaguf@users.noreply.github.com>
2023-03-05 23:50:05 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in storedResources"
+      :key="res.key"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resource/LearningResource.vue';
+export default {
+  components: {
+    LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guild',
+          title: 'Official guild',
+          description: 'The official vue js documentation,',
+          link: 'https://vuejs.org/v2/guide/',
+        },
+        {
+          id: 'Google',
+          title: 'Google',
+          description: 'The official vue js documentation,',
+          link: 'https://vuejs.org/v2/guide/',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
